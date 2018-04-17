c03145fbbf7a6196b9529f236295fa65ab948742
Darin Sikanic <darin.sikanic@gmail.com>
2018-04-17 18:38:43 +1000

    Simplified adding links

--- mn_topo/core/mn_topology.py
+++ mn_topo/core/mn_topology.py
@@ -34,28 +34,9 @@ class MininetTopology(object):
 
     def add_links(self):
         for link in self.topology.links:
-            src_name = link['source']
-            dst_name = link['destination']
-            # All of this is because we don't want to add a link to an interface
-            source = None
-            sw_src_index = self.topology.find_index_by_value('switches', 'name', src_name)
-            if sw_src_index is not None:
-                source = self.topology.switches[sw_src_index]
-            else:
-                host_src_index = self.topology.find_index_by_value('hosts', 'name', src_name)
-                source = self.topology.hosts[host_src_index]
-
-            destination = None
-            sw_dst_index = self.topology.find_index_by_value('switches', 'name', dst_name)
-            if sw_dst_index is not None:
-                destination = self.topology.switches[sw_dst_index]
-            else:
-                host_dst_index = self.topology.find_index_by_value('hosts', 'name', dst_name)
-                destination = self.topology.hosts[host_dst_index]
-
-            if ('type' not in source or source['type'] == 'ovs') \
-                    and ('type' not in destination or destination['type'] == 'ovs'):
-                self.mininet_topology.addLink(source['name'], destination['name'])
+            if link['source'] in self.mininet_topology.nodes() \
+                    and link['destination'] in self.mininet_topology.nodes():
+                self.mininet_topology.addLink(link['source'], link['destination'])
 
     def add_controllers(self):
         for controller in self.topology.controllers:
