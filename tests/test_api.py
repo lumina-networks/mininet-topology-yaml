26080e872acd5cd9936671c7e08f704cd29f6028
Darin Sikanic <darin.sikanic@gmail.com>
2018-04-17 10:15:34 +1000

    Adding api tests and removing unecessary __main__.py file

---
+++ tests/test_api.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+from mn_topo import api
+from mn_topo.core.constants import DEFAULT_CONTROLLERS
+
+def read(filename):
+    output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+    with open(output_file, 'r') as fin:
+        return fin.read()
+
+
+def test_create_table_topology(tmpdir):
+    p = tmpdir.mkdir("sub").join("topo.yml")
+    api.create_table_topology(p.strpath, controllers=DEFAULT_CONTROLLERS)
+    content = p.read()
+    assert content == read('./mocks/mock_table_topo.yml')
+
+
+def test_create_dc_topology(tmpdir):
+    p = tmpdir.mkdir("sub").join("topo.yml")
+    api.create_dc_topology(p.strpath, controllers=DEFAULT_CONTROLLERS)
+    content = p.read()
+    assert content == read('./mocks/mock_dc_topo.yml')
+
